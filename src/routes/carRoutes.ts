a82dc97c981dd085d9e17dfc5a02133c13d9e611
Isaac <isaacluiz0808@gmail.com>
2025-03-05 15:34:49 -0300

    Percebendo o erro que foi feito em carRoutes.ts, as funções devem estar nos controllers e não nas rotas

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import Car from "../models/Car";
-import { io } from "../../server"
 
 const router = express.Router();
 
-// Criar um novo carro
-export const createCar = async (req: Request, res: Response) => {
+// Rota para criar um novo carro
+router.post("/", async (req, res) => {
   try {
     const car = new Car(req.body);
     await car.save();
@@ -13,83 +12,47 @@ export const createCar = async (req: Request, res: Response) => {
   } catch (err) {
     res.status(500).json({ error: "Erro ao criar o carro", details: err });
   }
-};
+});
 
-// Controlador para listar todos os carros
-export const getAllCars = async (req: Request, res: Response) => {
+// Rota para listar todos os carros
+router.get("/", async (req, res) => {
   try {
     const cars = await Car.find();
     res.json(cars);
   } catch (err) {
     res.status(500).json({ error: "Erro ao buscar os carros", details: err });
   }
-};
+});
 
-// Controlador para atualizar um carro pelo ID
-export const updateCarById = async (req: Request, res: Response) => {
+// Rota para atualizar um carro pelo ID
+router.put("/:id", async (req: any, res: any) => {
   try {
     const { id } = req.params;
     const updatedCar = await Car.findByIdAndUpdate(id, req.body, { new: true });
     if (!updatedCar) {
       return res.status(404).json({ error: "Carro não encontrado" });
     }
-    res.json(updatedCar);
+    return res.json(updatedCar);
   } catch (err) {
-    res.status(500).json({ error: "Erro ao atualizar o carro", details: err });
+    return res.status(500).json({ error: "Erro ao atualizar o carro", details: err });
   }
-};
+});
 
-// Controlador para deletar um carro pelo ID
-export const deleteCarById = async (req: Request, res: Response) => {
+
+// Rota para deletar um carro pelo ID
+router.delete("/:id", async (req: any, res: any) => {
   try {
     const { id } = req.params;
     const deletedCar = await Car.findByIdAndDelete(id);
+
     if (!deletedCar) {
       return res.status(404).json({ error: "Carro não encontrado" });
     }
+
     res.json({ message: "Carro deletado com sucesso" });
   } catch (err) {
     res.status(500).json({ error: "Erro ao deletar o carro", details: err });
   }
-};
-
-
-// Controlador para fazer um lance em um carro
-export const bidOnCar = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const { user, bidAmount } = req.body;
-    
-    const car = await Car.findById(id);
-    if (!car) {
-      return res.status(404).json({ error: "Carro não encontrado" });
-    }
-    
-    if (bidAmount <= car.currentPrice) {
-      return res.status(400).json({ error: "O lance deve ser maior que o valor atual" });
-    }
-    
-    car.bids.push({ user, bidAmount, timestamp: new Date() });
-    car.currentPrice = bidAmount;
-    await car.save();
-
-    // Notificar todos os clientes sobre o novo lance
-    io.emit("newBid", { carId: car._id, user, bidAmount });
-
-    res.json({ message: "Lance feito com sucesso", car });
-  } catch (err) {
-    res.status(500).json({ error: "Erro ao fazer um lance", details: err });
-  }
-};
-
-// Controlador para listar apenas carros vendidos / Buscar carros vendidos
-export const getSoldCars = async (req: Request, res: Response) => {
-  try {
-    const soldCars = await Car.find({ status: "Vendido" });
-    res.json(soldCars);
-  } catch (err) {
-    res.status(500).json({ error: "Erro ao buscar os carros vendidos", details: err });
-  }
-};
+});
 
 export default router;
